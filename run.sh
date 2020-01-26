6abf16290bfd30d3f977dc6a3c126706cf85c928
JamiJ <jami.jernvall@gmail.com>
2020-01-26 20:55:32 +0100

    Changed the links and paths to correct ones.

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ sudo apt-get -y install git salt-minion
 
 echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
-git clone https://github.com/JamiJ/RTMP-phone
-cd RTMP-phone
+git clone https://github.com/JamiJ/nginx-automatic
+cd nginx-automatic
 sudo bash highstate.sh
 
-echo "Configuring Desktop"
+echo "Configuring Keyboard & Localization - For Finnish use"
 setxkbmap fi
 sudo timedatectl set-timezone Europe/Helsinki
 git config --global credential.helper "cache --timeout=3600"
